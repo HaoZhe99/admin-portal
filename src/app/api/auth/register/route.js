260a2e8c67637d9e89192f96be3878f1c8af8515
hz <haozhe@optimumtech.my>
2024-12-30 12:00:13 +0800

    update

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,25 +1,32 @@
-import { isEmpty } from 'lodash'
+import { isEmpty } from "lodash";
 
-import { pool } from '../../../../utils/db/db_connection'
+import { pool } from "../../../../utils/db/db_connection";
+import CryptoJS from "crypto-js";
 
 export async function POST(req) {
   try {
-    const { username, email, password } = await req.json()
+    const { username, email, password } = await req.json();
 
     if (isEmpty(username) || isEmpty(email) || isEmpty(password)) {
-      return new Response(JSON.stringify({ message: 'All fields are required' }), { status: 400 })
+      return new Response(
+        JSON.stringify({ message: "All fields are required" }),
+        { status: 400 },
+      );
     }
 
-    const result = await pool.query('INSERT INTO users (name,  email,password) VALUES ($1, $2, $3) RETURNING *', [
-      username,
-      email,
-      password
-    ])
+    const hashedPassword = CryptoJS.SHA256(password).toString();
 
-    return new Response(JSON.stringify(result.rows[0]), { status: 201 })
+    const result = await pool.query(
+      "INSERT INTO users (name,  email,password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, hashedPassword],
+    );
+
+    return new Response(JSON.stringify(result.rows[0]), { status: 201 });
   } catch (error) {
-    console.error('Error creating product:', error)
+    console.error("Error creating product:", error);
 
-    return new Response(JSON.stringify({ message: 'Internal Server Error' }), { status: 500 })
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 }
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,25 @@
 import { pool } from "./db/db_connection";
+import CryptoJS from "crypto-js";
 
 export async function getUserFromDb(email, pwHash) {
   try {
-    const result = await pool.query(
-      `SELECT * FROM users WHERE email = $1 AND password = $2`,
-      [email, pwHash],
-    );
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
-    // If a user is found, return the first row; otherwise, return null
-    return result.rows[0] || null;
+    if (result.rows.length === 0) {
+      throw new Error("Invalid credentials");
+    }
+
+    const user = result.rows[0];
+
+    const hashedInputPassword = CryptoJS.SHA256(pwHash).toString();
+
+    if (hashedInputPassword !== user.password) {
+      throw new Error("Invalid credentials");
+    }
+
+    return { id: user.id, name: user.name, email: user.email };
   } catch (error) {
     console.error("Error fetching user from database:", error);
     throw new Error("Database query failed");
