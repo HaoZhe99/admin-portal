f0b8d8a64e868b2b7bd7a59bd7c002084b154bfc
hz <haozhe@optimumtech.my>
2024-12-31 15:49:18 +0800

    update

--- src/app/(dashboard)/product/page.js
+++ src/app/(dashboard)/product/page.js
@@ -78,9 +78,11 @@ const ProductsPage = () => {
       <div className="flex justify-between items-end pb-4">
         <h1 className="text-2xl font-bold text-black">Product Listing</h1>
 
-        <button className="inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide transition duration-300 rounded focus-visible:outline-none justify-self-center whitespace-nowrap bg-emerald-50 text-emerald-500 hover:bg-emerald-100 hover:text-emerald-600 focus:bg-emerald-200 focus:text-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-100 disabled:text-emerald-400 disabled:shadow-none">
-          <a href="/product/create">Create</a>
-        </button>
+        <a href="/product/create">
+          <button className="inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide transition duration-300 rounded focus-visible:outline-none justify-self-center whitespace-nowrap bg-emerald-50 text-emerald-500 hover:bg-emerald-100 hover:text-emerald-600 focus:bg-emerald-200 focus:text-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-100 disabled:text-emerald-400 disabled:shadow-none">
+            Create
+          </button>
+        </a>
       </div>
 
       {loading ? (
